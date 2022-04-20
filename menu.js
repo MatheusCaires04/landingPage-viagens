6b45c77e6dc930e5a1a60f70a852faddd051e6df
MatheusCaires04 <matheus-binI@hotmail.com>
2022-04-20 15:38:06 -0300

    Projeto Finalizado

---
+++ menu.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector(".menu__hamburguer");
+
+menu.addEventListener("click", function () {
+  const menuFlex = document.querySelector(".menu");
+  menuFlex.classList.toggle("active");
+
+  
+});
